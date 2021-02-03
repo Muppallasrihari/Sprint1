d6986e6f385fb102f3ea7c2d5d64de59d9a2965d
Nidhi291 <72148816+Nidhi291@users.noreply.github.com>
2021-02-03 19:08:15 +0530

    DietDAO Updated

--- healthyfy/src/main/java/com/cg/healthify/daos/DietDAO.java
+++ healthyfy/src/main/java/com/cg/healthify/daos/DietDAO.java
@@ -3,12 +3,12 @@ package com.cg.healthify.daos;
 import com.cg.healthify.pojo.DietPlan;
 import com.cg.healthify.pojo.Login;
 
-public interface DietDAO {
-	public DietPlan save_diet_plan(DietPlan diet);
+public interface DietDAO {	
+	public DietPlan saveDietPlan(DietPlan diet);
 
-	public DietPlan update_diet_plan(DietPlan diet);
+	public DietPlan updateDietPlan(DietPlan diet);
 
-	public DietPlan remove_diet_plan(DietPlan diet);
+	public DietPlan removeDietPlan(DietPlan diet);
 
-	public DietPlan customer_diet_plan_data(int id);
+	public DietPlan getCustomerDietPlan(int id);
 }
