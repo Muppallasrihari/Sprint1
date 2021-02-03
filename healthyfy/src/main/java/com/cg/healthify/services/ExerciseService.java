33c43d386c5c2398be719db85cc5d30dcf16fd31
fusebox123 <76993141+fusebox123@users.noreply.github.com>
2021-02-03 15:59:49 +0530

    ExerciseService final added

--- healthyfy/src/main/java/com/cg/healthify/services/ExerciseService.java
+++ healthyfy/src/main/java/com/cg/healthify/services/ExerciseService.java
@@ -1,15 +1,27 @@
 package com.cg.healthify.services;
 
+import java.util.List;
+
+import com.cg.healthify.exceptions.NegativeIdException;
 import com.cg.healthify.pojo.Exercise;
 
+/**
+ * Business logic Interface
+ * @Joel Jacob
+ *
+ */
 public interface ExerciseService {
 	
 	public Exercise addExercise(Exercise exercise);
 
-	public Exercise updateExercise(Exercise exercise);
+	public Exercise updateExercise(Exercise exercise) throws NegativeIdException,NullPointerException;
 
 	public Exercise deleteExercise(Exercise exercise);
 
-	public Exercise findExerciseData(Exercise exercise);
+	public List<Exercise> findAll();
+	
+	public Exercise findById(int id) throws NullPointerException, NegativeIdException;
+	
+	public void validateId(int id) throws NegativeIdException;
 
 }
