c9711d3b974d54098f9234284481141b3ccf2d3e
RAGHUVEER SINGH <53135497+Raghuveersingh-2097@users.noreply.github.com>
2021-02-03 15:18:08 +0530

    Update CustomerServiceImpl.java

--- healthyfy/src/main/java/com/cg/healthify/services/CustomerServiceImpl.java
+++ healthyfy/src/main/java/com/cg/healthify/services/CustomerServiceImpl.java
@@ -3,134 +3,131 @@ package com.cg.healthify.services;
 import java.util.List;
 import java.util.Scanner;
 
+import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
-import com.cg.healthify.daos.CustomerDAOImpl;
-import com.cg.healthify.exceptions.NoRecordFoundException;
-import com.cg.healthify.main.MainMenu;
-import com.cg.healthify.pojo.Customer;
-import com.cg.healthify.pojo.Login;
-import com.cg.healthify.pojo.Payment;
-import com.cg.healthify.util.DBUtil;
+import com.cg.healthyfy.daos.CustomerDAO;
+import com.cg.healthyfy.daos.CustomerDAOImpl;
+import com.cg.healthyfy.domain.Customer;
+import com.cg.healthyfy.domain.Login;
+import com.cg.healthyfy.exceptions.NoRecordFoundException;
+import com.cg.healthyfy.menu.MainMenu;
+import com.cg.healthyfy.util.SameUtilContainer;
 
-public class CustomerServiceImpl extends DBUtil implements CustomerService {
-	Customer cust = new Customer();
-	CustomerDAOImpl daoimpl = new CustomerDAOImpl();
-	Login login = new Login();
-	Scanner sc = new Scanner(System.in);
 
-	public Customer addUser(Customer cust) {
-		daoimpl.saveCustomer(cust);
+
+public class CustomerServiceImpl implements CustomerService{
+	CustomerDAO custdao;
+	EntityManager em=SameUtilContainer.emf.createEntityManager();
+
+
+	public CustomerServiceImpl() {
+		custdao=new CustomerDAOImpl();
+	}
+	Login login=new Login();
+
+	public Customer adduser(Customer cust) {
+		custdao.saveCustomerDetail(cust);
 		return cust;
 	}
-
-	public Customer updateUser(Customer cust) {
-		/*int io = 0;
+	public Customer updateuser(Customer cust){
+		int io1=0;
+		int ini=cust.getId();
 		try {
-			Query query = em.createQuery("from Customer");
-			List<Login> loginm = query.getResultList();
-			for (Login i : loginm) {
-				if (i.getId() == cust.getId()) {
-					io++;
+			Query query=em.createQuery("from Customer");
+			List<Customer> customer= query.getResultList();
+			for(Customer i:customer) {
+				if(i.getId()==ini) {
+					io1++;
 					break;
 				}
-			}
-			Validate(io);
-		} catch (NoRecordFoundException m) {
-			System.out.println(m);
+			}  
+			Validate(io1);
 		}
-		if (io == 1) {
-			*/
-			daoimpl.updateCustomer(cust);
-		
-		return cust;
-
-	}
-
-	public Customer deleteUser(Customer customer) throws NoRecordFoundException {
-		// TODO Auto-generated method stub
-		int io = 0;
+		catch(NoRecordFoundException m) {
+			System.out.println(m);
+		}if(io1==1) {
+			return custdao.updateCustomerDetails(cust);
+		}
+		return cust;}
+	public int deleteUser(int id){
+		int io1=0;
+		int ini=id;
 		try {
-			Query query = em.createQuery("from Login");
-			List<Login> loginm = query.getResultList();
-			for (Login i : loginm) {
-				if (i.getId() == cust.getId()) {
-					io++;
+			Query query=em.createQuery("from Customer");
+			List<Customer> customer= query.getResultList();
+			for(Customer i:customer) {
+				if(i.getId()==ini) {
+					io1++;
 					break;
 				}
-			}
-			Validate(io);
-		} catch (NoRecordFoundException m) {
+			}  
+			Validate(io1);
+		}
+		catch(NoRecordFoundException m) {
 			System.out.println(m);
+		}if(io1==1) {
+			custdao.cancelPlan(ini);
 		}
-		if (io == 1) {
-			login.setId(cust.getId());
-			daoimpl.cancelPlan(customer);
-		}
-		return customer;
-
+		return io1;
 	}
-
-	public int findUser(int id) throws NoRecordFoundException {
-		// TODO Auto-generated method stub
-		int io = 0;
+	@Override
+	public List<Customer> findAll() {
+		custdao.findAll();
+		return null;
+	}
+	@Override
+	public int findUser(int i)throws NoRecordFoundException{
+		int io1=0;
 		try {
-			
-			Query query = em.createQuery("from Login");
-			List<Login> loginm = query.getResultList();
-			for (Login i : loginm) {
-				if (i.getId() == cust.getId()) {
-					io++;
+			Query query=em.createQuery("from Customer");
+			List<Customer> customer= query.getResultList();
+			for(Customer ii:customer) {
+				if(ii.getId()==i) {
+					io1++;
 					break;
 				}
-			}
-			Validate(io);
-		} catch (NoRecordFoundException m) {
+			}  
+			Validate(io1);
+		}
+		catch(NoRecordFoundException m) {
 			System.out.println(m);
+		}if(io1==1) {
+			custdao.findByUser(i);
 		}
-		if (io == 1) {
-			daoimpl.findByUser(id);
-		}
-		return id;
-
+		return io1;
 	}
 
-	private void Validate(int io) throws NoRecordFoundException {
-		if (io == 0) {
+	static void Validate(int io)throws NoRecordFoundException{
+		if(io==0) {
 			throw new NoRecordFoundException("No Records Found");
 		}
 	}
 
-	
-	public void loginAuthentication(int id, String password) throws NoRecordFoundException {
-		
-		int io = 0;
-		em.getTransaction().begin();
-		Query query = em.createQuery("from Login");
-		List<Login> loginm = query.getResultList();
-		for (Login i : loginm) {
-			if (i.getId() == id && i.getPassword().equals(password)) {
+	public void loginAuthentication() throws NoRecordFoundException {
+		Scanner sc=new Scanner(System.in);
+		EntityManager em=SameUtilContainer.emf.createEntityManager();
+		System.out.println("Enter your ID:");
+		int ini=sc.nextInt();
+		System.out.println("Enter Password:");
+		String password=sc.next();
+		int io=0;
+
+		Query query=em.createQuery("from Login");
+		List<Login> loginm= query.getResultList();
+		for(Login i:loginm) {
+			if(i.getId()==ini && i.getPassword().equals(password)) {
 				io++;
 				break;
 			}
 		}
-		if (io == 0) {
+		if(io==0) {
 			System.out.println("Invalid User!!");
-		} else {
-			MainMenu main = new MainMenu();
-			em.getTransaction().commit();
-			main.mainMenu();
-
+		}
+		else {
+			MainMenu mains=new MainMenu();
+			mains.main_menu();		  
 		}
 	}
-
-	
-
-	@Override
-	public List<Customer> findAll() {
-		// TODO Auto-generated method stub
-		daoimpl.findAll();
-		return null;
-	}
-
 }
+
