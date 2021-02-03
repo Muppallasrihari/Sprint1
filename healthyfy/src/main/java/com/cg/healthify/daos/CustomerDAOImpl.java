774ff1e25486ce81a2581a06717514be8cfe9925
RAGHUVEER SINGH <53135497+Raghuveersingh-2097@users.noreply.github.com>
2021-02-03 15:12:10 +0530

    Update CustomerDAOImpl.java

--- healthyfy/src/main/java/com/cg/healthify/daos/CustomerDAOImpl.java
+++ healthyfy/src/main/java/com/cg/healthify/daos/CustomerDAOImpl.java
@@ -1,91 +1,100 @@
-package com.cg.healthify.daos;
-
+//package com.cg.healthyfy.daos;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.persistence.Query;
 
-import com.cg.healthify.exceptions.NoRecordFoundException;
-import com.cg.healthify.pojo.Customer;
-import com.cg.healthify.pojo.Login;
-import com.cg.healthify.util.DBUtil;
+import com.cg.healthyfy.domain.Customer;
+import com.cg.healthyfy.domain.Login;
+import com.cg.healthyfy.exceptions.NoRecordFoundException;
+import com.cg.healthyfy.util.SameUtilContainer;
 
-public class CustomerDAOImpl extends DBUtil implements CustomerDAO {
 
+public class CustomerDAOImpl implements CustomerDAO{
+	//EntityManagerFactory emf=Persistence.createEntityManagerFactory("JPA-PU");	
 	@Override
-	public Customer saveCustomer(Customer cust) {
-		Login login=new Login();	
-		  em.getTransaction().begin();
-	      em.persist(cust);
-	      login.setId(cust.getId());
-		  login.setPassword(cust.getName()+cust.getId());
-	      em.persist(login);
-	      System.out.println("Welcome Our New User: "+cust.getName()+"\nYour LoginId is: "+login.getId()+"\nYour Password is: "+login.getPassword());
-	      em.getTransaction().commit();
-	      return cust;
-	}
-
+	public Customer saveCustomerDetail(Customer in){
+	 EntityManager em=SameUtilContainer.emf.createEntityManager();	
+	  Login log=new Login();	
+	  em.getTransaction().begin();
+      em.persist(in);
+      log.setId(in.getId());
+	  log.setPassword(in.getName()+in.getId());
+      em.persist(log);
+      System.out.println("Welcome Our New User: "+in.getName()+"\nYour LoginId is: "+log.getId()+"\nYour Password is: "+log.getPassword());
+      em.getTransaction().commit();
+     // em.close();
+      return in;
+}	
 	@Override
-	public Customer updateCustomer(Customer cust) {
-		String newContact = cust.getContact();
-		String newMedical = cust.getMedicalCondition();
-		String newAllergic = cust.getAllergicTo();
+	public Customer updateCustomerDetails(Customer cust) {
+		EntityManager em=SameUtilContainer.emf.createEntityManager();
+		String newContact=cust.getContact();
+		String newMedical=cust.getMedicalCondition();
+		String newAllergic=cust.getAllergicTo();
 		em.getTransaction().begin();
-		cust = em.find(Customer.class, cust.getId());
+		cust=em.find(Customer.class, cust.getId());
 		cust.setContact(newContact);
 		cust.setMedicalCondition(newMedical);
 		cust.setAllergicTo(newAllergic);
 		em.persist(cust);
 		System.out.println("Your Details Are Updated....");
 		em.getTransaction().commit();
-		return cust;
+	//	em.close();
+		return cust;		
 	}
-
 	@Override
-	public Customer cancelPlan(Customer cust) throws NoRecordFoundException {
+	public Customer cancelPlan(int id)throws NoRecordFoundException{
+		EntityManager em=SameUtilContainer.emf.createEntityManager(); 
+		Login log=new Login();
 		em.getTransaction().begin();
-		cust = em.find(Customer.class, cust.getId());
+		Customer cust=em.find(Customer.class, id);
+		log=em.find(Login.class, id);
 		em.remove(cust);
-		// em.remove(log);
+		em.remove(log);
 		System.out.println("Your Plan is Cancelled");
 		em.getTransaction().commit();
+		em.close();
 		return cust;
-	}
-
+		}
+	
 	@Override
 	public List<Customer> findAll() {
 		// TODO Auto-generated method stub
-		Query query = em.createQuery("from Customer");
+	EntityManager em=SameUtilContainer.emf.createEntityManager();
+		Query query=em.createQuery("from Customer");
 		@SuppressWarnings("unchecked")
-		List<Customer> list = (List<Customer>) query.getResultList();
-		for (Customer c : list) {
-			System.out.println("Customer ID: " + c.getId());
-			System.out.println("Name: " + c.getName());
-			System.out.println("Gender: " + c.getGender());
-			System.out.println("Date Of Birth: " + c.getDob());
-			System.out.println("Contact Number:" + c.getContact());
-			System.out.println("Medical Condition: " + c.getMedicalCondition() + "\nAllergy To: " + c.getAllergicTo());
+		List<Customer> list=(List<Customer>)query.getResultList();
+		for(Customer c:list) {
+			System.out.println("Customer ID: "+c.getId());
+			System.out.println("Name: "+c.getName());
+			System.out.println("Gender: "+c.getGender());
+			System.out.println("Date Of Birth: "+c.getDob());
+			System.out.println("Contact Number:"+c.getContact());
+			System.out.println("Medical Condition: "+c.getMedicalCondition()+"\nAllergy To: "+c.getAllergicTo());
 			System.out.println("\n");
 		}
 		return list;
 	}
-
 	@Override
 	public Customer findByUser(int i) throws NoRecordFoundException {
 		// TODO Auto-generated method stub
-		Customer cust = new Customer();
-		cust = em.find(Customer.class, i);
+	EntityManager em=SameUtilContainer.emf.createEntityManager();
+		Customer cust=new Customer();
+		cust=em.find(Customer.class, i);
 		em.getTransaction().begin();
-		System.out.println("Customer ID: " + cust.getId());
-		System.out.println("Name: " + cust.getName());
-		System.out.println("Gender: " + cust.getGender());
-		System.out.println("Date Of Birth: " + cust.getDob());
-		System.out.println("Contact Number:" + cust.getContact());
-		System.out
-				.println("Medical Condition: " + cust.getMedicalCondition() + "\nAllergy To: " + cust.getAllergicTo());
-		em.getTransaction().commit();
+		System.out.println("Customer ID: "+cust.getId());
+		System.out.println("Name: "+cust.getName());
+		System.out.println("Gender: "+cust.getGender());
+		System.out.println("Date Of Birth: "+cust.getDob());
+		System.out.println("Contact Number:"+cust.getContact());
+		System.out.println("Medical Condition: "+cust.getMedicalCondition()+"\nAllergy To: "+cust.getAllergicTo());
+		em.getTransaction().commit(); 
+    //    em.close();
+
 		return cust;
 	}
-
-	
-
 }
+
