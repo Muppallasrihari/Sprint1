373aa4a3c346506535bfc7d2243a1b742abe9d52
VINOTRAJ <76986189+VINOTRAJ@users.noreply.github.com>
2021-02-03 16:59:55 +0530

    Add files via upload
    Updated WeightLogException

---
+++ healthyfy/src/main/java/com/cg/healthify/exceptions/InvalidIdException.java
@@ -0,0 +1,14 @@
+package com.cg.healthyfy.exception;
+
+public class InvalidIdException extends Exception {
+	
+	public InvalidIdException ()
+	{
+		
+	}
+	public InvalidIdException(String Msg)
+	{
+		super(Msg);
+	}
+
+}
