d2cc9433622baa9bf8cf610a9c5dccced3dc8c4c
fusebox123 <76993141+fusebox123@users.noreply.github.com>
2021-02-03 14:30:35 +0530

    Exercise menu added

--- healthyfy/src/main/java/com/cg/healthify/main/ExerciseMenu.java
+++ healthyfy/src/main/java/com/cg/healthify/main/ExerciseMenu.java
@@ -12,15 +12,15 @@ public class ExerciseMenu {
 	ExerciseServiceImpl exerciseimpl = new ExerciseServiceImpl();
 
 	void menu() {
-		System.out.println("1. ADD THE EXERCISE PLAN");
-		System.out.println("2. DELETE THE PLAN");
-		System.out.println("3. UPDATE THE PLAN");
-		System.out.println("4. SHOW ALL THE PLANS");
+		System.out.println("1. ADD EXERCISE");
+		System.out.println("2. DELETE EXERCISE");
+		System.out.println("3. UPDATE EXERCISE");
+		System.out.println("4. DISPLAY ALL EXERCISES");
 		System.out.println("5. EXIT");
 	}
 
 	public void exerciseMenu() {
-//menu();
+
 		do {
 			System.out.println("Enter the Choice: ");
 			menu();
@@ -36,12 +36,12 @@ public class ExerciseMenu {
 				exerciseimpl.addExercise(exercise);
 				break;
 			case 2:
-				System.out.println("Please Confirm your Id to remove your exercise plan: ");
+				System.out.println("Please Confirm your Id to remove the exercise plan: ");
 				exercise.setId(sc.nextInt());
 				exerciseimpl.deleteExercise(exercise);
 				break;
 			case 3:
-				System.out.println("Please Confirm your ID to update Exercise plan ");
+				System.out.println("Please Confirm your ID to update the Exercise plan ");
 				exercise.setId(sc.nextInt());
 				System.out.println("Enter the Exercise type: ");
 				exercise.setExerciseType(sc.next());
@@ -58,10 +58,10 @@ public class ExerciseMenu {
 				System.exit(0);
 				break;	
 			default:
-				System.exit(0);
+				System.out.println("Wrong Choice");
 			}
-			System.out.println("Want to Continue as user.");
+			System.out.println("Want to Continue as user? y/n");
 			ContChoice = sc.next();
-		} while (ContChoice.equalsIgnoreCase("Y"));
+		} while (ContChoice.equalsIgnoreCase("y"));
 	}
 }
