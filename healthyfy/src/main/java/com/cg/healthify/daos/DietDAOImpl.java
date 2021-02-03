7b1b4dc55504f58ea92cf595bb67fc6f0935ed13
Nidhi291 <72148816+Nidhi291@users.noreply.github.com>
2021-02-03 17:12:03 +0530

    Update DietDAOImpl.java

--- healthyfy/src/main/java/com/cg/healthify/daos/DietDAOImpl.java
+++ healthyfy/src/main/java/com/cg/healthify/daos/DietDAOImpl.java
@@ -3,40 +3,47 @@ package com.cg.healthify.daos;
 import com.cg.healthify.pojo.DietPlan;
 import com.cg.healthify.pojo.Login;
 import com.cg.healthify.util.DBUtil;
-
-public class DietDAOImpl extends DBUtil {
-
-	public void savePlan(DietPlan diet, Login login) {
-		diet = new DietPlan(diet.getId(), diet.getSlots(), diet.getTypeOfFood(), diet.getProteinRatio());
+public class DietDAOImpl extends DBUtil implements DietDAO {
+	public DietPlan save_diet_plan(DietPlan diet) {
+		EntityManager em=DBUtil.emf.createEntityManager();
+		diet = new DietPlan(diet.getId(), diet.getSlots(), diet.getTypeOfFood(), diet.getProteinRatio(),
+				diet.getRatioOfFat(), diet.getRatioOfcarbs(), diet.getTotal());
 		em.getTransaction().begin();
 		em.persist(diet);
 		System.out.println("Diet Plan Saved.");
 		em.getTransaction().commit();
+		return diet;
 	}
 
-	public void updatePlan(DietPlan diet) {
-		String new_slot = diet.getSlots();
+	public DietPlan update_diet_plan(DietPlan diet) {
+		EntityManager em=DBUtil.emf.createEntityManager();
+		String newSlot = diet.getSlots();
 		em.getTransaction().begin();
 		diet = em.find(DietPlan.class, diet.getId());
-		diet.setSlots(new_slot);
+		diet.setSlots(newSlot);
 		em.persist(diet);
 		System.out.println("Your diet plan is Updated...");
 		em.getTransaction().commit();
+		return diet;
 	}
 
-	public void removePlan(DietPlan diet) {
+	public DietPlan remove_diet_plan(DietPlan diet) {
+		EntityManager em=DBUtil.emf.createEntityManager();
 		diet = em.find(DietPlan.class, diet.getId());
 		em.getTransaction().begin();
 		em.remove(diet);
 		System.out.println("Your Current Diet Plan is Removed");
 		em.getTransaction().commit();
+		return diet;
 	}
 
-	public void customerDietPlan(DietPlan diet) {
+	public DietPlan customer_diet_plan_data(int id) {
+		EntityManager em=DBUtil.emf.createEntityManager();
+		DietPlan dietPlanInfo = new DietPlan();
+		dietPlanInfo = em.find(DietPlan.class, id);
 		em.getTransaction().begin();
-		diet = em.find(DietPlan.class, diet.getId());
-		System.out.println("DietID: " + diet.getId() + "\n" + "Slots: " + diet.getSlots() + "\n" + "FOOD TYPE"
-				+ diet.getTypeOfFood() + "\n" + "PROTIEN RATIO" + diet.getProteinRatio());
 		em.getTransaction().commit();
+		return dietPlanInfo;
 	}
+
 }
