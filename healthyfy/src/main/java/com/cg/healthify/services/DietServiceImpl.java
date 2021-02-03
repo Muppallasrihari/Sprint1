0d7c23ec3de164fd8cf53f5088ef8c07c6eab05e
Nidhi291 <72148816+Nidhi291@users.noreply.github.com>
2021-02-03 17:24:03 +0530

    Update DietServiceImpl.java

--- healthyfy/src/main/java/com/cg/healthify/services/DietServiceImpl.java
+++ healthyfy/src/main/java/com/cg/healthify/services/DietServiceImpl.java
@@ -11,38 +11,107 @@ import com.cg.healthify.exceptions.NoRecordFoundException;
 import com.cg.healthify.pojo.DietPlan;
 import com.cg.healthify.pojo.Login;
 import com.cg.healthify.util.DBUtil;
-
-public class DietServiceImpl extends DBUtil {
+public class DietServiceImpl extends DBUtil implements DietService {
 	DietPlan diet = new DietPlan();
 	DietDAOImpl dietdao = new DietDAOImpl();
 	Login log = new Login();
 	Scanner sc = new Scanner(System.in);
 
 	public DietPlan addDiet(DietPlan diet) {
+		EntityManager em=DBUtil.emf.createEntityManager();
 		int io = 0;
-		try {
-			
-			Query query = em.createQuery("from Login");
-			List<Login> loginm = query.getResultList();
-			for (Login i : loginm) {
-				if (i.getId() == diet.getId()) {
-					io++;
-					break;
-				}
+
+		Query query = em.createQuery("from LoginInfo");
+		List<Login> loginm = query.getResultList();
+		for (Login i : loginm) {
+			if (i.getId() == diet.getId()) {
+				io++;
+				break;
 			}
-			Validate(io);
-		} catch (NoRecordFoundException m) {
-			System.out.println(m);
 		}
+
 		if (io == 1) {
 			if (diet.getTypeOfFood().equalsIgnoreCase("NONVEG")) {
 				diet.setProteinRatio(DietPlanConstants.non_veg_protien_ratio);
+				diet.setRatioOfFat(DietPlanConstants.nonVegFatRatio);
+				diet.setRatioOfcarbs(DietPlanConstants.nonVegCarbsRatio);
 			} else {
 				diet.setProteinRatio(DietPlanConstants.veg_protien_ratio);
+				diet.setRatioOfFat(DietPlanConstants.vegFatRatio);
+				diet.setRatioOfcarbs(DietPlanConstants.vegFatRatio);
 			}
-			dietdao.savePlan(diet, log);
+			diet.setTotal(diet.getRatioOfcarbs() + diet.getRatioOfFat());
+			dietdao.save_diet_plan(diet);
+		} else {
+			System.out.println("User record already exists");
+		}
+
+		return diet;
+	}
+
+	public DietPlan updateDiet(DietPlan diet) {
+		EntityManager em=DBUtil.emf.createEntityManager();
+		int io = 0;
+		Query query = em.createQuery("from LoginInfo");
+		List<Login> loginm = query.getResultList();
+		for (Login i : loginm) {
+			if (i.getId() == diet.getId()) {
+				io++;
+				break;
+			}
+		}
+
+		if (io == 1) {
+			DietPlan userExistingDietInfo = findDietData(diet.getId());
+			if (userExistingDietInfo != null) {
+				userExistingDietInfo.setSlots(diet.getSlots());
+				dietdao.update_diet_plan(userExistingDietInfo);
+			}
+		} else {
+			System.out.println("No Record Found");
+		}
+
+		return diet;
+	}
+
+	public DietPlan deleteDiet(DietPlan diet) {
+		EntityManager em=DBUtil.emf.createEntityManager();
+		int io = 0;
+		Query query = em.createQuery("from LoginInfo");
+		List<Login> loginm = query.getResultList();
+		for (Login i : loginm) {
+			if (i.getId() == diet.getId()) {
+				io++;
+				break;
+			}
+		}
+		if (io == 1) {
+			dietdao.remove_diet_plan(diet);
+		} else {
+			System.out.println("No Record Found");
 		}
 		return diet;
+	}
+
+	public DietPlan findDietData(int id) {
+		EntityManager em=DBUtil.emf.createEntityManager();
+		int io = 0;
+		DietPlan dietPlanInfo = new DietPlan();
+
+		Query query = em.createQuery("from LoginInfo");
+		List<Login> loginm = query.getResultList();
+		for (Login i : loginm) {
+			if (i.getId() == id) {
+				io++;
+				break;
+			}
+		}
+
+		if (io == 1) {
+			dietPlanInfo = dietdao.customer_diet_plan_data(id);
+		}
+
+		return dietPlanInfo;
 	}
 
 	static void Validate(int io) throws NoRecordFoundException {
@@ -50,71 +119,4 @@ public class DietServiceImpl extends DBUtil {
 			throw new NoRecordFoundException("No Records Found");
 		}
 	}
-
-	public DietPlan updateDiet(DietPlan diet) {
-		int io = 0;
-		try {
-			
-			Query query = em.createQuery("from Login");
-			List<Login> loginm = query.getResultList();
-			for (Login i : loginm) {
-				if (i.getId() == diet.getId()) {
-					io++;
-					break;
-				}
-			}
-			Validate(io);
-		} catch (NoRecordFoundException m) {
-			System.out.println(m);
-		}
-		if (io == 1) {
-			dietdao.updatePlan(diet);
-		}
-		return diet;
-	}
-
-	public DietPlan deleteDiet(DietPlan diet) {
-		int io = 0;
-		try {
-			
-			Query query = em.createQuery("from Login");
-			List<Login> loginm = query.getResultList();
-			for (Login i : loginm) {
-				if (i.getId() == diet.getId()) {
-					io++;
-					break;
-				}
-			}
-			Validate(io);
-		} catch (NoRecordFoundException m) {
-			System.out.println(m);
-		}
-		if (io == 1) {
-			dietdao.removePlan(diet);
-		}
-		return diet;
-	}
-
-	public DietPlan findDietData(DietPlan diet) {
-		int io = 0;
-		try {
-			
-			Query query = em.createQuery("from Login");
-			List<Login> loginm = query.getResultList();
-			for (Login i : loginm) {
-				if (i.getId() == diet.getId()) {
-					io++;
-					break;
-				}
-			}
-			Validate(io);
-		} catch (NoRecordFoundException m) {
-			System.out.println(m);
-		}
-		if (io == 1) {
-			dietdao.customerDietPlan(diet);
-		}
-		return diet;
-	}
-
 }
