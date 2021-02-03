89a7594b0b74c5ebfd7b6360f8502fdec9e12a19
VINOTRAJ <76986189+VINOTRAJ@users.noreply.github.com>
2021-02-03 16:50:05 +0530

    Update WeightLogDAO.java
    Updated WeightLogDAo

--- healthyfy/src/main/java/com/cg/healthify/daos/WeightLogDAO.java
+++ healthyfy/src/main/java/com/cg/healthify/daos/WeightLogDAO.java
@@ -4,9 +4,9 @@ import com.cg.healthify.pojo.WeightLog;
 
 public interface WeightLogDAO {
 
-	public WeightLog addUserWeight(WeightLog weight);
-	public WeightLog updateUserWeight(WeightLog weight);
-	public WeightLog deleteUserWeight(WeightLog weight);
-	public WeightLog findUserWeight(WeightLog weight);
+	public WeightLog addUserWeight(WeightLog weg);
+	public WeightLog updateUserWeight(WeightLog weg);
+	public WeightLog deleteUserWeight(WeightLog weg);
+	public WeightLog findUserWeight(WeightLog weg);
 
 }
